990c8704e52c6b2d8c06bf934ef6b34dcaa95393
Dominic Walsh <djcwalsh94@gmail.com>
2022-10-25 14:21:47 +0200

    aaaa"
    exit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  # See the new task form
+  get '/tasks/new', to: 'tasks#new'
+  # create new task
+  post '/tasks', to: 'tasks#create'
+  #show a specific task
+  get '/tasks/:id', to: 'tasks#show'
+  #verb #url         #controller #action
 end
